5229ca6aad89048671772aed20d0c6a6eb881bde
dineshbalans <dineshbalansrinivasan@gmail.com>
2024-08-13 17:26:15 +0530

    env commit

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,24 +7,31 @@ import { CustomInterceptors } from './common/interceptors/response.interceptor';
 import { CustomExceptionFilter } from './common/exceptions/errors.exception';
 import { setupSwagger } from './config/swagger.config';
 import { MailService } from '@org/utils';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+
     BigInt.prototype['toJSON'] = function () {
       return this.toString();
     };
     // app.useGlobalPipes(new ValidationPipe(validationPipeConfig));
     // app.useGlobalInterceptors(new CustomInterceptors());
     // app.useGlobalFilters(new CustomExceptionFilter());
-    app.enableCors({origin:"*"});
+    app.enableCors({ origin: '*' });
 
     app.use(helmet());
     setupSwagger(app);
-    await app.listen(8080);
+    await app.listen(process.env.BACKEND_PORT);
+    // await app.listen(configService.get<number>('BACKEND_PORT'));
+
+    Logger.log(`Application is running on: ${await app.getUrl()}`);
   } catch (error) {
     console.log(error);
-    Logger.log(error)
+    Logger.log(error);
   }
 }
 bootstrap();
