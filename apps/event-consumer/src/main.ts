a17185caecb3e8ba08ca3562f6a68614b874d1cc
Aswin2667 <aswin96777@gmail.com>
2024-10-13 17:35:38 +0530

    fixed docker file

--- apps/event-consumer/src/main.ts
+++ apps/event-consumer/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
+  const configService = app.get(ConfigService);
 
   BigInt.prototype['toJSON'] = function () {
     return this.toString();
   };
-
-  await app.listen(8082);
+  console.log(process.env.BACKEND_PORT);
+  await app.listen(process.env.PORT || 8082);
 }
 bootstrap();
